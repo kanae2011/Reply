b0ad15ff9b1901eaad35a4ad0af249159b62f49b
kanae2011 <62246184+kanae2011@users.noreply.github.com>
2021-04-23 17:41:24 +0900

    Update ReplyController.java

--- ReplyController.java
+++ ReplyController.java
@@ -1,6 +1,5 @@
 package org.zerock.board.controller;
 
-import java.net.URLEncoder;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +9,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.board.service.ReplyService;
 import org.zerock.board.vo.ReplyVO;
 
@@ -34,7 +34,10 @@ public class ReplyController {
 	private ReplyService service;
 	
 	//1.게시판 댓글 목록(보기 포함) -검색 /list.do - get
-	@GetMapping(value = "/list.do",produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_PROBLEM_JSON_UTF8_VALUE})
+	@GetMapping(value = "/list.do",
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_UTF8_VALUE})
 	
 	// ResponseEntity : 실행 상태 코드와 함께 실행결과를 클라이언트에서 전달 할 때 사용하는 객체 
 	public ResponseEntity<List<ReplyVO>> list(
@@ -45,48 +48,71 @@ public class ReplyController {
 		//댓글에 대한 페이지 정보
 		PageObject replyPageObject = new PageObject(replyPage,replyPerPageNum);
 		log.info("list().replyPageObject : " + replyPageObject + "......");
-		return new ResponseEntity<> (service.list(replyPageObject,no),HttpStatus.OK);
+		
+		return new ResponseEntity<>(service.list(replyPageObject, no), HttpStatus.OK);
+	      // /WEB-INF/views + /board/list + .jsp -> servlet-context.xml 정보
+		
+	}
+	
+	//게시판 댓글 등록 처리
+	@PostMapping(value = "/write.do",
+//			consumes = "application/json", 
+			consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE},
+			produces = {"application/text; charset=utf-8"})
+//			produces = {MediaType.TEXT_PLAIN_VALUE})
+	//2.게시판 작성 /write.do - post
+	public ResponseEntity<String> write(@RequestBody ReplyVO vo)throws Exception {
+		log.info("write.vo()" + vo);
+		
+		//db에 데이터 저장
+		service.write(vo);
+		return new ResponseEntity<String>("댓글이 등록되었습니다.",HttpStatus.OK);
 		
 	}
 	
 	
-	@PostMapping("/write.do")
-	//2.게시판 작성 /write.do - post
-	public String write(ReplyVO vo,int perPageNum, RedirectAttributes rttr) throws Exception {
-		log.info("write.vo()" + vo);
-		service.write(vo);
-		rttr.addFlashAttribute("msg","글 작성 성공");
-		return "redirect:list.do?perPageNum=" + perPageNum;
-	}
-	
-	
-	@PostMapping("/update.do")
+	@PatchMapping(value = "/update.do",
+			consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE},
+			produces = {"application/text; charset=utf-8"})
 	//4-2.게시판 글수정 /update.do - post
-	public String update(ReplyVO vo,RedirectAttributes rttr,PageObject pageObject) throws Exception {
-		log.info("update().vo" + vo);
+	public ResponseEntity<String> update(@RequestBody ReplyVO vo) throws Exception {
+		log.info("update().vo : " + vo);
 		
 		int result = service.update(vo);
-		if(result == 0)throw new Exception("Board update Fasle -정보 확인 요망");
-		log.info("update().result" + result);
-		rttr.addFlashAttribute("msg", "글 수정 성공");
-		return "redirect:view.do?no=" + vo.getNo() + "&inc=0" 
-			+ "&page=" + pageObject.getPage() 
-			+ "&perPageNum=" + pageObject.getPerPageNum()
-			+ "&key=" + pageObject.getKey()
-			//URL로 요청되는 경우 서버의 한글이 적용되므로 UTF-8로 Encode시킴
-			+ "&word=" + URLEncoder.encode(pageObject.getWord(),"utf-8") 
-			;
+		
+		//전달 되는 데이터의 선언
+		String msg = "댓글 수정을 완료했습니다.";
+		HttpStatus status = HttpStatus.OK;
+		
+		if(result == 0) {
+			msg = "댓글 수정 중 오류가 발생했습니다.";
+			status = HttpStatus.NOT_MODIFIED;
+		}
+		log.info("update().msg : " + msg);
+		
+		return new ResponseEntity<String>(msg,status);
+			
 	}
 	
-	@DeleteMapping("/delete.do")
+	@DeleteMapping(value = "/delete.do",
+			consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE},
+			produces = {"application/text; charset=utf-8"})
 	//5.게시판 글삭제 delete방식
-	public String delete(ReplyVO vo,int perPageNum,RedirectAttributes rttr) throws Exception {
+	public ResponseEntity<String> delete(@RequestBody ReplyVO vo) throws Exception {
 		log.info("delete().vo:"  + vo);
 		
 		int result = service.delete(vo);
-		//result가 0이면 비밀번호가 틀림
-		if(result == 0)throw new Exception("Board delete false - 정보확인 요망");
-		rttr.addFlashAttribute("msg", "글 삭제 성공");
-		return "redirect:list.do?perPageNum=" + perPageNum;
+		
+		//전달 되는 데이터의 선언
+		String msg = "댓글 삭제를 완료했습니다.";
+		HttpStatus status = HttpStatus.OK;
+		
+		if(result == 0) {
+			msg = "댓글 삭제 중 오류가 발생했습니다.";
+			status = HttpStatus.NOT_MODIFIED;
+		}
+		log.info("delete().msg : " + msg);
+		
+		return new ResponseEntity<String>(msg,status);
 	}
 }
